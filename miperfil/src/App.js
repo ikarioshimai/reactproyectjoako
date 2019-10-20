168b55a0ef1d98dc0b064d9dcbc59408eae40243
ikarioshimai <diosdelaceite000@gail.com>
2019-10-19 19:46:53 -0500

    NUEVO

---
+++ miperfil/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './component/layout/index';
+function App() {
+  return (
+    <div className="container-padre-app">
+<Layout/>
+    </div>
+  );
+}
+
+export default App;
